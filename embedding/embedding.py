35411248134fb86cdcf1990eb8677109a45430c0
Kaito Shigyo / 執行凱斗 <132188853+furedea@users.noreply.github.com>
2023-11-25 19:49:55 +0900

    add embedding.py connecting with S3 and retriving
    embedding.py connects with S3, downloads vector data from it, and retrives what a prompter requests.

---
+++ embedding/embedding.py
@@ -0,0 +1,23 @@
+import pickle
+
+import boto3
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+
+import dotenv_setting
+
+# DynamoDBからデータを取得
+dynamodb = boto3.resource("dynamodb", region_name="ap-northeast-1")
+table = dynamodb.Table("ScrapingPhoneStatus")
+response = table.scan()
+data = response["Items"][:5]
+
+embedding = OpenAIEmbeddings()
+
+# データをベクトル化
+with open("vectorstore.pkl", "wb") as f:
+    for item in data:
+        # 各カラムを一つのテキストとして連結
+        text = " ".join(str(value) for value in item.values())
+        vector = FAISS.from_texts(text, embedding)
+        pickle.dump(vector, f)
