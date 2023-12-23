2f4e96d39b025b8054c836b63093b5af2610a287
Kaito Shigyo / 執行凱斗 <132188853+furedea@users.noreply.github.com>
2023-12-23 21:49:01 +0900

    Update chatbot.py changing Document to dict

--- langchain-local/chatbot.py
+++ langchain-local/chatbot.py
@@ -1,4 +1,5 @@
 import os
+import re
 import tempfile
 from typing import Final
 
@@ -15,26 +16,53 @@ FAISS_FILE_PATH: Final = "index.faiss"
 PKL_FILE_PATH: Final = "index.pkl"
 
 
+class Retrieval:
+    def __init__(self) -> None:
+        # boto3 S3クライアントの作成
+        s3 = boto3.client("s3")
+
+        # 一時ディレクトリの作成
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            # S3からファイルをダウンロード
+            s3.download_file(BUCKET_NAME, FAISS_FILE_PATH, os.path.join(tmp_dir, FAISS_FILE_PATH))
+            s3.download_file(BUCKET_NAME, PKL_FILE_PATH, os.path.join(tmp_dir, PKL_FILE_PATH))
+            # FAISSインデックスのロード
+            embedding = OpenAIEmbeddings()
+            self.vector_store = FAISS.load_local(tmp_dir, embedding)
+        self.retriever = self.vector_store.as_retriever(search_kwargs={"k": 3})
+
+    def retrieve(self, query: str) -> dict[str, str]:  # 情報を検索して返す
+        docs = self.retriever.get_relevant_documents(query)
+        return docs
+        # pprint(docs)
+        # print("---------------------------------------------------")
+
+        # llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+        # qa = RetrievalQA.from_chain_type(llm=llm, retriever=self.retriever)
+        # answer = qa(query)
+        # return answer
+
+
+def convert_to_dict(input_str: str) -> dict[str, str]:
+    result_dict = {}
+    pairs = re.findall(r"([^:|]+):([^|]*)", input_str)
+    for pair in pairs:
+        if pair[1].strip():
+            key, value = pair
+            result_dict[key.strip()] = value.strip()
+    return result_dict
+
+
 if __name__ == "__main__":
-    # boto3 S3クライアントの作成
-    s3 = boto3.client("s3")
+    query = "iPhone 12 Pro Maxに近いスペックのスマートフォンについて教えてください"
 
-    # 一時ディレクトリの作成
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        # S3からファイルをダウンロード
-        s3.download_file(BUCKET_NAME, FAISS_FILE_PATH, os.path.join(tmp_dir, FAISS_FILE_PATH))
-        s3.download_file(BUCKET_NAME, PKL_FILE_PATH, os.path.join(tmp_dir, PKL_FILE_PATH))
-        # FAISSインデックスのロード
-        embedding = OpenAIEmbeddings()
-        vector_store = FAISS.load_local(tmp_dir, embedding)
+    retrieval = Retrieval()
+    answers = retrieval.retrieve(query)
 
-    query = "iPhone 11 Pro Maxに近いスペックのスマートフォンについて教えてください"
-    retriever = vector_store.as_retriever()
-    docs = retriever.get_relevant_documents(query)
-    print(docs)
+    answer_list = [
+        convert_to_dict(answer.page_content) for answer in answers
+    ]  # Document型のリストをdict型のリストに変換
+    phone_list = [answer["機種"] for answer in answer_list]
+
     print("---------------------------------------------------")
-
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    qa = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
-    answer = qa(query)
-    print(answer)
+    print(phone_list)
