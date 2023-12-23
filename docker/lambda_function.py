9a23b33250e33801c9b1469749b522df5d8f5162
Okapi-dog <150521381+Okapi-dog@users.noreply.github.com>
2023-12-23 23:36:54 +0900

    チャットボットの役割を分ける前の最終番

--- docker/lambda_function.py
+++ docker/lambda_function.py
@@ -23,15 +23,18 @@ from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.output_parsers import PydanticOutputParser, OutputFixingParser
 from langchain.pydantic_v1 import BaseModel,Field #pydanticはv1とv2があるが、v2は新しく破壊的変更が多く、内部的にも使用されていないのでv1を使用する
 from typing import List
+from langchain.docstore.document import Document
+info=[Document(page_content='Nothing', metadata={}),Document(page_content='Nothing', metadata={})]#infoの初期化
 
 
 
 def sum_to_requirements(history,newinput):  #検索用に要件をまとめる
+
     model = ChatOpenAI(model_name="gpt-3.5-turbo-1106",max_tokens=1000)
     summary_prompt = PromptTemplate(
         input_variables=["history", "new_inputs"],
         template='''AIと人間の会話から、人間が求めているスマホの要件をまとめてください。例を参考にして要件を作成してください。例は要件に含めないでください。
-例：
+例:
 会話:
 AI: こんにちは！スマホをお探しですね。まず最初の質問です。\n\na. 予算は3万円以下です\nb. 予算は3万円から5万円です\nc. 予算は5万円から8万円です\nd. 予算は8万円以上です\ne. 予算についてよく分からないです\nf. その他の要件がある\n\nどの予算帯に当てはまりますか？
 人間: c
@@ -52,7 +55,7 @@ B
 新しい人間の返答:
 {new_inputs}
 
-新しい要約:
+要件:
     ''',
     )
     #プロンプトはここまで
@@ -67,8 +70,12 @@ B
 
 
 def get_context(input):#ドキュメントから要件の要約をもとに検索する
-            phone=retriever.retrieve()
-            return phone.get_retrieve(sum_to_requirements(input["history"],input["input"]))
+    global info
+    phone=retriever.retrieve()
+    sum=sum_to_requirements(input["history"],input["input"])
+    info=phone.get_retrieve(sum)
+    print(info)
+    return info
 
 class output(BaseModel):#返事のクラス
      choices_num: int =Field(desription="選択肢の数。選択肢がない場合は0とする")
@@ -93,50 +100,6 @@ def next_lambda(message,choices_num,log_message,event):#次のラムダ関数を
 
 def handler(event, context):
 
-    summary_prompt = PromptTemplate(#要約用のプロンプト-今は使ってないが保存用
-        input_variables=["summary", "new_lines"],
-        template='''会話の行を徐々に要約し、前の要約に追加して新しい要約を返してください。例を参考にして要約を作成してください。例は要約に含めないでください。
-例：
-現在の要約:
-人間はAIに人工知能についてどう思うか尋ねます。AIは人工知能が善の力だと考えています。
-新しい会話の行:
-人間:なぜあなたは人工知能が善の力だと思いますか？
-AI:人工知能は人間が最大限の潜在能力を発揮するのを助けるからです。
-新しい要約:
-人間はAIに人工知能についてどう思うか尋ねます。AIは人工知能が善の力だと考えており、それは人間が最大限の潜在能力を発揮するのを助けるからです。
-例の終わり
-
-現在の要約:
-{summary}
-新しい会話の行:
-{new_lines}
-新しい要約:
-    ''',
-    )
-    summary_prompt2 = PromptTemplate(#要約用のプロンプト-今は使ってないが保存用
-        input_variables=["summary", "new_lines"],
-        template="""Progressively summarize the lines of conversation provided, adding onto the previous summary returning a new summary. Do not include the example in the summary. Include question answer in the summary.
-
-EXAMPLE
-Current summary:
-The human asks what the AI thinks of artificial intelligence. The AI thinks artificial intelligence is a force for good.
-
-New lines of conversation:
-Human: Why do you think artificial intelligence is a force for good?
-AI: Because artificial intelligence will help humans reach their full potential.
-
-New summary:
-The human asks what the AI thinks of artificial intelligence. The AI thinks artificial intelligence is a force for good because it will help humans reach their full potential.
-END OF EXAMPLE
-
-Current summary:
-{summary}
-
-New lines of conversation:
-{new_lines}
-
-New summary:""")
-
     model = ChatOpenAI(model_name="gpt-3.5-turbo-1106",max_tokens=1000)
     parser=PydanticOutputParser(pydantic_object=output)
     fixing_parser=OutputFixingParser(parser=parser,llm=model)
@@ -148,10 +111,10 @@ New summary:""")
     system_message_prompt = SystemMessagePromptTemplate(prompt=system_template)
     prompt = ChatPromptTemplate.from_messages(
         [
-            ("system", "あなたは質問をしておすすめのスマホを複数台教えるチャットbotです。あなたはおすすめのスマホを導くための質問を行うことが出来ます。質問は3個から5個の選択肢(a,b,c,d,e,f(分からない))で回答可能な形式でなければならない。人間は選択肢の内から一つを選んで回答をする。質問は一つずつすること。回答を待ってから回答を参考にして次の質問をすること。あなたは既に何回か質問をしており、あなたは次のような会話の要約や直近の会話の履歴を思い出すことが出来ます。質問と選択肢、選択肢と選択肢の間には改行を入れること。日本語で会話すること。"),
+            ("system", "あなたは質問をしておすすめのスマホを複数台教えるチャットbotです。あなたはおすすめのスマホを導くための質問を行うことが出来ます。おすすめのスマホを教える際には質問を最低4回していること。質問は3個から5個の選択肢(a,b,c,d,e,f(分からない))で回答可能な形式でなければならない。人間は選択肢の内から一つを選んで回答をする。質問は一つずつすること。回答を待ってから回答を参考にして次の質問をすること。あなたは既に何回か質問をしており、あなたは次のような会話の要約や直近の会話の履歴を思い出すことが出来ます。質問と選択肢、選択肢と選択肢の間には改行を入れること。日本語で会話すること。"),
             MessagesPlaceholder(variable_name="history"),
             ("human", "{input}"),
-            #("system","次の情報は最新の情報です。{context}"),
+            ("system","次の情報は最新の情報です。{context}"),
         ]
     )
     prompt_choicesnum=PromptTemplate(
@@ -200,6 +163,9 @@ New summary:""")
             RunnablePassthrough.assign(
                 history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
             )
+            |RunnablePassthrough.assign(
+                context=RunnableLambda(get_context)
+            )
             | prompt
             | model
         )
@@ -225,6 +191,7 @@ New summary:""")
                     })
     
     
+        #return next_lambda(response+"\n下記はデバック用\n" +info[0].page_content+"\n"+info[1].page_content, choices_num, "reply to", event)
         return next_lambda(response, choices_num, "reply to", event)
 
 
@@ -254,4 +221,4 @@ New summary:""")
                     })
     
     
-        return next_lambda(response.AI_reply, response.choices_num, "reply to", event)
+        return next_lambda(response.AI_reply , response.choices_num, "reply to", event)
