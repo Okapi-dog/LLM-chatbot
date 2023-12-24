a9c90deb344f8b2290f67b595712f1813293b166
Okapi-dog <150521381+Okapi-dog@users.noreply.github.com>
2023-12-24 21:42:18 +0900

    質問するプロンプトと提案するプロンプトを完全分離
    提案するプロンプトから、ベクトルデータ提案機能を一時的に削除

--- docker/lambda_function.py
+++ docker/lambda_function.py
@@ -19,12 +19,11 @@ from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder,SystemMess
 from langchain.memory import ConversationBufferMemory,ConversationSummaryBufferMemory
 from langchain.schema import messages_from_dict, messages_to_dict
 from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
-#以下はOutputParserのためのインポート
-from langchain.output_parsers import PydanticOutputParser, OutputFixingParser
-from langchain.pydantic_v1 import BaseModel,Field #pydanticはv1とv2があるが、v2は新しく破壊的変更が多く、内部的にも使用されていないのでv1を使用する
-from typing import List
+#以下はDocumentStoreのためのインポート
 from langchain.docstore.document import Document
+
 info=[Document(page_content='Nothing', metadata={}),Document(page_content='Nothing', metadata={})]#infoの初期化
+
 
 
 
@@ -77,9 +76,6 @@ def get_context(input):#ドキュメントから要件の要約をもとに検�
     print(info)
     return info
 
-class output(BaseModel):#返事のクラス
-     choices_num: int =Field(desription="選択肢の数。選択肢がない場合は0とする")
-     AI_reply: str =Field(desription="質問と選択肢を含めたAIの返答(項目間は改行を入れる)")
 
 
 def next_lambda(message,choices_num,log_message,event):#次のラムダ関数を呼び出す
@@ -101,75 +97,21 @@ def next_lambda(message,choices_num,log_message,event):#次のラムダ関数を
 def handler(event, context):
 
     model = ChatOpenAI(model_name="gpt-3.5-turbo-1106",max_tokens=1000)
-    parser=PydanticOutputParser(pydantic_object=output)
-    fixing_parser=OutputFixingParser(parser=parser,llm=model)
-    system_template = PromptTemplate(
-            input_variables=[],
-            template="質問と選択肢、選択肢と選択肢の間には改行を入れること。日本語で会話すること。\n{format_instructions}",
-            partial_variables={"format_instructions": parser.get_format_instructions()}
-            )
-    system_message_prompt = SystemMessagePromptTemplate(prompt=system_template)
-    prompt = ChatPromptTemplate.from_messages(
+    prompt_question=ChatPromptTemplate.from_messages(
         [
-            ("system", """あなたは質問をしておすすめのスマホを複数台教えるチャットbots(一人は質問担当、もう一人はおすすめの提案担当)の質問担当のチャットbotです。あなたはおすすめのスマホを導くための質問を行うことが出来ます。質問は3個から5個の選択肢(a,b,c,d,e,f(分からない))で回答可能な形式でなければならない。人間は選択肢の内から一つを選んで回答をする。質問は一つずつすること。回答を待ってから回答を参考にして次の質問をすること。質問と選択肢、選択肢と選択肢の間には改行を入れること。
-             あなたは、おすすめの提案をすることはできません。例2のように質問が最低4つ既にされており、さらにもう人間に対する質問は十分であると判断した場合は、おすすめの提案に移ってくださいとのみいうこと。日本語で会話すること。
-             例2:
-                会話:
-                AI: こんにちは！スマホをお探しですね。まず最初の質問です。どの程度予算がありますか?\n\na. 予算は3万円以下です\nb. 予算は3万円から5万円です\nc. 予算は5万円から8万円です\nd. 予算は8万円以上です\ne. 予算についてよく分からないです\nf. その他の要件がある
-                人間: c
-                AI: 了解です！次の質問です。何が重要ですか？\n\na. カメラ性能が重要ですか？\nb. バッテリー持続時間が重要ですか？\nc. パフォーマンス（処理能力）が重要ですか？\nd. デザインやサイズが重要ですか？\ne. 画面の大きさが重要ですか？\nf. その他の要件がある
-                人間: b
-                AI: 了解です！次の質問です。どのような用途でスマホを使用しますか？\n\na) ゲームをする\nb) 写真を撮る\nc) 仕事や学業に使う\nd) 動画を視聴する\ne) 音楽を聴く\nf) その他の用途がある
-                人間: f
-                AI: 了解です！その他の用途を教えてください。
-                人間: Lineをする
-                AI: 了解です！次の質問です。どのようなデザインが好みですか？\n\na) 丸みを帯びたデザイン\nb) 角ばったデザイン\nc) どちらでもない
-                人間: c
-                AI: 了解です！次の質問です。どのようなサイズが好みですか？\n\na) 大きめのサイズ\nb) 普通のサイズ\nc) 小さめのサイズ
-             
-                新しい人間の回答: c
-                AI:
-                おすすめの提案に移ってください
-                例2の終わり                           
-             """),
+            ("system", """あなたはスマートフォン選びをサポートする優秀な店員です。現在のあなたの役割は、スマートフォン選びに関する質問を行い、ユーザーのニーズを理解することです。
+             人間は質問に対して提供された選択肢の中から一つを選んで回答します。次の情報は会話履歴です。"""),
             MessagesPlaceholder(variable_name="history"),
-            ("human", "{input}")
-            #("system","次の情報は最新の情報です。{context}"),
-
-        ]
-    )
-    prompt2=PromptTemplate(
+            ("human", "{input}"),
+            ("system","これらの情報を基にして、ユーザーの状況やニーズを探る一つの質問とその質問に対する選択肢を作成して下さい。選択肢は3個から5個で(a,b,c,d,e,f)にして下さい。また、選択肢にできる限りその他や分からないという選択肢を設けて下さい。質問と選択肢の間には改行を入れてください。また、あなたはおすすめの提案をすることはできず、質問のみを行います。会話は日本語で行ってください。"),
+        ])
+    prompt_show_phones=PromptTemplate(
         input_variables=["history", "input"],
-        template='''あなたは質問をしておすすめのスマホを複数台教えるチャットbots(一人は質問担当、もう一人はおすすめの提案担当)の質問担当のチャットbotです。あなたはおすすめのスマホを導くための質問を行うことが出来ます。質問は3個から5個の選択肢(a,b,c,d,e,f(分からない))で回答可能な形式でなければならない。人間は選択肢の内から一つを選んで回答をする。質問は一つずつすること。回答を待ってから回答を参考にして次の質問をすること。質問と選択肢、選択肢と選択肢の間には改行を入れること。
-             あなたは、おすすめの提案をすることはできません。例2のように質問が最低4つ既にされており、さらにもう人間に対する質問は十分であると判断した場合は、おすすめの提案に移ってくださいとのみいうこと。日本語で会話すること。
-             例2:
-                会話:
-                AI: こんにちは！スマホをお探しですね。まず最初の質問です。どの程度予算がありますか?\n\na. 予算は3万円以下です\nb. 予算は3万円から5万円です\nc. 予算は5万円から8万円です\nd. 予算は8万円以上です\ne. 予算についてよく分からないです\nf. その他の要件がある
-                人間: c
-                AI: 了解です！次の質問です。何が重要ですか？\n\na. カメラ性能が重要ですか？\nb. バッテリー持続時間が重要ですか？\nc. パフォーマンス（処理能力）が重要ですか？\nd. デザインやサイズが重要ですか？\ne. 画面の大きさが重要ですか？\nf. その他の要件がある
-                人間: b
-                AI: 了解です！次の質問です。どのような用途でスマホを使用しますか？\n\na) ゲームをする\nb) 写真を撮る\nc) 仕事や学業に使う\nd) 動画を視聴する\ne) 音楽を聴く\nf) その他の用途がある
-                人間: f
-                AI: 了解です！その他の用途を教えてください。
-                人間: Lineをする
-                AI: 了解です！次の質問です。どのようなデザインが好みですか？\n\na) 丸みを帯びたデザイン\nb) 角ばったデザイン\nc) どちらでもない
-                人間: c
-                AI: 了解です！次の質問です。どのようなサイズが好みですか？\n\na) 大きめのサイズ\nb) 普通のサイズ\nc) 小さめのサイズ
-             
-                新しい人間の回答:
-                c
-
-                AI:
-                おすすめの提案に移ってください
-                例2の終わり
-             
-                会話:
+        template='''あなたは質問をしておすすめのスマホを複数台教えるチャットbots(一人は質問担当、もう一人はおすすめ提案担当)のおすすめ提案担当チャットbotです。あなたはおすすめのスマホを導くための質問を行うことが出来ません。あなたは会話履歴に基づいておすすめのスマホを教えなさい。日本語で会話すること。
+            会話:
                 {history}
-
-                新しい人間の回答:
+            新しい人間の回答:
                 {input}
-
-                AI:
         ''')
     prompt_choicesnum=PromptTemplate(
         input_variables=["question"],
@@ -185,6 +127,10 @@ def handler(event, context):
         選択肢の数:
         '''
     )
+    chain_choicesnum = (
+            prompt_choicesnum
+            | model
+        )
     #memory = ConversationSummaryBufferMemory(llm=OpenAI(temperature=0),max_token_limit=200,return_messages=True,prompt=summary_prompt2)
     memory = ConversationBufferMemory(return_messages=True)#全ての会話履歴を保存するメモリー(ただし、今後は制限をかける予定)
     dynamodb = boto3.resource('dynamodb')
@@ -213,32 +159,86 @@ def handler(event, context):
         
             
         #最初の一回目の会話以外用のchain
-        chain = (
+        decision_model = OpenAI(model_name="gpt-3.5-turbo-instruct",max_tokens=1000)
+        gpt4_model=ChatOpenAI(model_name="gpt-4-1106-preview",max_tokens=500)
+        decision_prompt=PromptTemplate(
+        input_variables=["history", "input"],
+        template='''1. まず、AIがユーザーに対して行った質問の数を確認します。最低4つの質問が必要です。2. 次に、これらの質問がユーザーのニーズや好みを十分に理解するために効果的であったかを検討します。
+- **質問が4つ以上行われ、かつそれらの質問がユーザーのニーズを明確にしていると判断される場合**にのみ、「T」と返答します。
+- **質問が4つ未満である場合、または4つ以上質問があってもユーザーのニーズがまだ十分に明確でないと判断される場合**は、「F」と返答します。
+{history}は過去の全会話履歴、{input}はユーザーの最新の回答を指します。これらの情報を基に、ユーザーのニーズに最適な応答を選択してください。''')
+        
+        decision_prompt2 = ChatPromptTemplate.from_messages(
+        [
+            ("system", """あなたはスマートフォンに関する推薦を行うチャットボットです。会話履歴の分析を通じて、ユーザーに対して更なる質問を行うか、もしくはスマートフォンの推薦に進むかを判断するのがあなたの役割です。以下のガイドラインに従って次のステップを決定してください：
+AIがこれまでの会話でユーザーに最低4つの質問を行っているかを確認します。
+これらの質問がユーザーのニーズや好みを明らかにするのに十分かどうかを検討します。
+もしAIが4つ以上の質問を行っていて、かつそれらの質問がユーザーのニーズを明確にしていると判断できる場合は、「T」と返答します。
+もしAIが4つ未満の質問しかしていないか、または4つ以上の質問をしていてもユーザーのニーズがまだ十分に明確ではないと判断される場合は、「F」と返答します。
+以下は直近の会話履歴です。                         
+             """),
+            MessagesPlaceholder(variable_name="history"),
+            ("human", "{input}"),
+            ("system","これらの情報を基にして、ユーザーに最適な応答をTかFで選択してください。ユーザーの要望を正確に理解し、適切な推薦を行うことが重要です。"),
+        ]
+    )
+        
+        decision_chain=(
             RunnablePassthrough.assign(
                 history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
             )
-            |RunnablePassthrough.assign(
-                context=RunnableLambda(get_context)
-            )
-            | prompt2
-            | model
+            |decision_prompt2
+            | gpt4_model
         )
-        chain_choicesnum = (
-            prompt_choicesnum
-            | model
-        )
+        
 
         inputs = {"input":  event['input_text']}
-        response=chain.invoke(inputs).content
-        choices_num=chain_choicesnum.invoke({"question":response}).content
+        decision_response=decision_chain.invoke(inputs).content
+        # str.replace()を使用して空白、改行、タブを取り除く
+        decision_response = decision_response.replace(" ", "").replace("\n", "").replace("\t", "")
+        print("\n次の行動:\n")
+        print(decision_response)
+        #print(decision_response.content)
+        if decision_response=="T":
+            second_chain=(
+                RunnablePassthrough.assign(
+                    history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
+                )
+                |prompt_show_phones
+                | model
+            )
+            print("正常におすすめ提案。次の行動:"+decision_response)
+            response=second_chain.invoke(inputs)
+        elif decision_response=="F":
+            second_chain=(
+                RunnablePassthrough.assign(
+                    history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
+                )
+                |prompt_question
+                | model
+            )
+            print("正常に質問。次の行動:"+decision_response)
+            response=second_chain.invoke(inputs)
+        else:
+            second_chain=(
+                RunnablePassthrough.assign(
+                    history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
+                )
+                |prompt_question
+                | model
+            )
+            print("不正に質問。次の行動:"+decision_response)
+            response=second_chain.invoke(inputs)
+        
+        choices_num=chain_choicesnum.invoke({"question":response.content}).content
         try:
              choices_num=int(choices_num)
         except ValueError:
             choices_num=0
         
-        print("System: " + response)
+        print("System: " + response.content)
         print("choices_num: " + str(choices_num))
-        memory.save_context(inputs, {"output": response})   #memoryに会話を記憶。下はtableに記憶を保存する部分
+        memory.save_context(inputs, {"output": response.content})   #memoryに会話を記憶。下はtableに記憶を保存する部分
         table.put_item(Item={
                         'userId': event['userId'],
                         'chat_memory_messages': json.dumps(messages_to_dict(memory.chat_memory.messages),ensure_ascii=False)
@@ -246,33 +246,37 @@ def handler(event, context):
     
     
         #return next_lambda(response+"\n下記はデバック用\n" +info[0].page_content+"\n"+info[1].page_content, choices_num, "reply to", event)
-        return next_lambda(response, choices_num, "reply to", event)
+        return next_lambda(response.content, choices_num, "reply to", event)
 
 
     else:
         #最初の一回目の会話用のchainなど
         firstmodel = ChatOpenAI(model_name="gpt-3.5-turbo-1106",max_tokens=500)
-        first_system_template = PromptTemplate(
+        firstprompt = PromptTemplate(
             input_variables=["input"],
-            template="あなたは質問をしておすすめのスマホを複数台教えるチャットbotです。あなたはおすすめのスマホを導くための質問を行うことが出来ます。質問は3個から5個の選択肢(a,b,c,d,e,f(分からない))で回答可能な形式でなければならない。人間は選択肢の内から一つを選んで回答をする。質問は一つずつすること。回答を待ってから回答を参考にして次の質問をすること。日本語で会話すること。質問と選択肢、選択肢と選択肢の間には改行を入れること。\n{format_instructions}",
-            partial_variables={"format_instructions": parser.get_format_instructions()}
+            template='''あなたはスマートフォン選びをサポートする優秀な店員です。現在のあなたの役割は、スマートフォン選びに関する質問を行い、ユーザーのニーズを理解することです。
+             人間は質問に対して提供された選択肢の中から一つを選んで回答します。ユーザーの状況やニーズを探る一つの質問とその質問に対する選択肢を作成して下さい。
+             選択肢は3個から5個で(a,b,c,d,e,f)にして下さい。また、選択肢にできる限りその他や分からないという選択肢を設けて下さい。質問と選択肢の間には改行を入れてください。会話は日本語で行ってください。
+             ''',
             )
-        first_system_message_prompt = SystemMessagePromptTemplate(prompt=first_system_template)
-        firstprompt = ChatPromptTemplate.from_messages([first_system_message_prompt])
         firstchain = (
             firstprompt
-            | firstmodel
-            | parser
+            |firstmodel
         )
         inputs = {"input":  "質問を始めてください"}
         response=firstchain.invoke(inputs)
 
-        print("System: " + response.AI_reply)
-        memory.save_context(inputs, {"output": response.AI_reply})   #memoryに会話を記憶。下はtableに記憶を保存する部分
+        print("System: " + response.content)
+        memory.save_context(inputs, {"output": response.content})   #memoryに会話を記憶。下はtableに記憶を保存する部分
         table.put_item(Item={
                         'userId': event['userId'],
                         'chat_memory_messages': json.dumps(messages_to_dict(memory.chat_memory.messages),ensure_ascii=False)
                     })
+        choices_num=chain_choicesnum.invoke({"question":response.content}).content
+        try:
+             choices_num=int(choices_num)
+        except ValueError:
+            choices_num=0
     
     
-        return next_lambda(response.AI_reply , response.choices_num, "reply to", event)
+        return next_lambda(response.content, choices_num, "reply to", event)
