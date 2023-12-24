a85526bdae34f3e29b8788c2eedaaee036794da7
Okapi-dog <150521381+Okapi-dog@users.noreply.github.com>
2023-12-24 22:37:44 +0900

    名前をわかりやすく変更した。

--- docker/retriever.py
+++ docker/retriever.py
@@ -1,6 +1,7 @@
 import os
 import tempfile
 from typing import Final
+import sys
 
 import boto3
 #from config import dotenv_setting  # api_keyの読み込み
@@ -11,10 +12,11 @@ from langchain.vectorstores import FAISS
 
 # S3からダウンロードするファイルの設定
 BUCKET_NAME: Final = "vector-store-s3"
+TABLE_NAME: Final = "ScrapingPhoneStatus"
 FAISS_FILE_PATH: Final = "index.faiss"
 PKL_FILE_PATH: Final = "index.pkl"
 
-class retrieve():
+class Retrieval():
     def __init__(self):
         # boto3 S3クライアントの作成
         s3 = boto3.client("s3")
@@ -22,21 +24,15 @@ class retrieve():
         # 一時ディレクトリの作成
         with tempfile.TemporaryDirectory() as tmp_dir:
             # S3からファイルをダウンロード
-            s3.download_file(BUCKET_NAME, FAISS_FILE_PATH, os.path.join(tmp_dir, FAISS_FILE_PATH))
-            s3.download_file(BUCKET_NAME, PKL_FILE_PATH, os.path.join(tmp_dir, PKL_FILE_PATH))
+            s3.download_file(BUCKET_NAME, os.path.join(TABLE_NAME,FAISS_FILE_PATH), os.path.join(tmp_dir, FAISS_FILE_PATH))
+            s3.download_file(BUCKET_NAME, os.path.join(TABLE_NAME,PKL_FILE_PATH), os.path.join(tmp_dir, PKL_FILE_PATH))
             # FAISSインデックスのロード
             embedding = OpenAIEmbeddings()
             self.vector_store = FAISS.load_local(tmp_dir, embedding)
         self.retriever = self.vector_store.as_retriever(search_kwargs={"k": 2})
 
-    def get_retrieve(self,query):#情報を検索して返す
-        docs = self.retriever.get_relevant_documents(query)
-        print(docs)
-        print("---------------------------------------------------")
-
-        llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-        qa = RetrievalQA.from_chain_type(llm=llm, retriever=self.retriever)
-        answer = qa(query)
+    def retrieve(self,query):#情報を検索して返す
+        answer = self.retriever.get_relevant_documents(query)
         return answer
     
     
@@ -44,6 +40,6 @@ class retrieve():
 
 if __name__ == "__main__":  #テスト用,importされた時には実行されない
     query = "iPhone 11 Pro Maxに近いスペックのスマートフォンについて教えてください"
-    a=retrieve()
-    answer=a.get_retrieve(query)
+    a=Retrieval()
+    answer=a.retrieve(query)
     print(answer)
