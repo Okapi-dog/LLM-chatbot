966efb763d0608f6c752f95747027fa83bb9a33a
Kaito Shigyo / 執行凱斗 <132188853+furedea@users.noreply.github.com>
2023-12-29 15:20:02 +0900

    Update embedding.py formatting for embedding

--- embedding/embedding.py
+++ embedding/embedding.py
@@ -1,28 +1,43 @@
+import re
+from pprint import pprint
 from typing import Final
 
 import boto3
-from config import dotenv_setting  # api_keyの読み込み
+from boto3.dynamodb.conditions import Attr
+from config import dotenv_setting  # noqa # api_keyの読み込み
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 
 BUCKET_NAME: Final = "vector-store-s3"
-TABLE_NAME: Final = "ScrapingPhoneStatus"
+TABLE_NAME: Final = "ScrapingPhoneStatus"  # Benchmarkデータをembeddingしたい場合はBenchmarkに変更
+
+
+class DynamoDB:
+    def __init__(self, table_name: str) -> None:
+        self.__dynamodb = boto3.resource("dynamodb", region_name="ap-northeast-1")
+        self.__table = self.__dynamodb.Table(table_name)
+
+    def fetch(self, phone_name: str | None = None) -> list[dict[str, str]]:
+        # DynamoDBから全データまたはphone_nameに合致するデータを取得
+        if phone_name is None:
+            return self.__table.scan()["Items"]
+        return self.__table.scan(FilterExpression=Attr("機種").eq(phone_name))["Items"]
+
+
+def format_for_embedding(key: str, value: str) -> str:
+    # 値が空文字列のペアは除外、キーが機種の場合はSIMフリーを省略
+    if not value:
+        return ""
+    if key == "機種":
+        formatted_value = re.sub(r" SIMフリー$", "", value)
+        return f"{key}:{formatted_value} | "
+    return f"{key}:{value} | "
 
 
 if __name__ == "__main__":
-    # boto3 S3クライアントとDynamoDBリソースの作成
-    dynamodb = boto3.resource("dynamodb", region_name="ap-northeast-1")
-    s3 = boto3.client("s3")
-
-    # DynamoDBからテキストデータを取得
-    table = dynamodb.Table(TABLE_NAME)
-    response = table.scan()
-    items = response["Items"]
-
-    # OpenAIEmbeddingsのインスタンスを作成
+    phones = DynamoDB(TABLE_NAME).fetch()
+    answers = ["".join(format_for_embedding(key, value) for key, value in phone_dict.items()) for phone_dict in phones]
     embedding = OpenAIEmbeddings()
-
-    item_str = [" ".join(f"{key}:{value}" for key, value in item.items()) for item in items]
-    vector_store = FAISS.from_texts(item_str, embedding)
+    vector_store = FAISS.from_texts(answers, embedding)
     # バイトデータを保存
-    vector_store.save_local("faiss")
+    vector_store.save_local("ScrapingPhoneStatus")
