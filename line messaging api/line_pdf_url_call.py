2607f66b32acc4db519c1c4886aefa41b932c506
Okapi Dog <150521381+Okapi-dog@users.noreply.github.com>
2024-01-21 23:03:19 +0900

    AWSの件さうk

--- line messaging api/line_pdf_url_call.py
+++ line messaging api/line_pdf_url_call.py
@@ -4,6 +4,20 @@ import os
 from linebot import LineBotApi
 from linebot.models import FlexSendMessage
 from urllib.parse import quote
+import json
+
+def send_text_to_line(message,event):#エラー時にテキストをLineで送信するラムダ関数を呼び出す
+    lambda_client = boto3.client('lambda')
+    #ARN of plain_text_output
+    next_function_name = 'arn:aws:lambda:ap-northeast-1:105837277682:function:plain_text_output'
+    response = lambda_client.invoke(
+        FunctionName=next_function_name,
+        InvocationType='Event',
+        Payload=json.dumps({'input_text': message, 'replyToken': event['replyToken'], 'userId': event['userId']} )
+    )
+
+
+
 
 def lambda_handler(event, context):
   # Extract the LINE user ID from the event object
@@ -12,96 +26,35 @@ def lambda_handler(event, context):
   
   if not userId:
     return {'statusCode': 400, 'body': 'LINE user ID not provided'}
+  if not replyToken:
+    return {'statusCode': 400, 'body': 'LINE replyToken not provided'}
   
   # Initialize a boto3 client for S3
   s3_client = boto3.client('s3')
   bucket_name = os.environ['BUCKET_NAME']  # Set your bucket name in Lambda's environment variables
   file_name=""
+  file_url=""
 
   try:
-    # List objects in the specified S3 bucket
-    response = s3_client.list_objects_v2(Bucket=bucket_name)
-    
-    if 'Contents' in response:
-      for item in response['Contents']:
-        file_name = item['Key']
-            
-        # Check if the file name contains the LINE user ID and is a PDF
-        if userId in file_name and file_name.endswith('.pdf'):
-          file_url = f"https://{bucket_name}.s3.amazonaws.com/{file_name}"
-          print("file_url:"+file_url)
-          print("file_urlエンコード:"+quote(file_url,safe=':/'))
+    s3_resource = boto3.resource('s3')
+    filtered_objects = s3_resource.Bucket(bucket_name).objects.filter(Prefix="スマホ紹介"+userId)
+    objects = list(filtered_objects)
+    if objects:
+      file_name = objects[0].key
     else:
-      file_url=""
-      
+      return send_text_to_line('''おすすめのスマホを提供する際にファイルシステムにおいてエラーが発生しました。申し訳ございません。
+      もう一度お試しいただくか、サポートにご連絡ください1''',event)
+    
+    file_url = f"https://{bucket_name}.s3.amazonaws.com/{file_name}"
+    print("file_url:"+file_url)
+    print("file_urlエンコード:"+quote(file_url,safe=':/'))
+    
   except Exception as e:
-    print("エラー発生")
-    return {'statusCode': 500, 'body': f'Error occurred: {str(e)}'}
-
+    print(f"エラーが発生しました: {e}")
+    return send_text_to_line('''おすすめのスマホを提供する際にファイルシステムにエラーが発生しました。申し訳ございません。
+      もう一度お試しいただくか、サポートにご連絡ください2''',event)
+  
   flex_message_contents = {
-    "type": "bubble",
-    "header": {
-      "type": "box",
-      "layout": "vertical",
-      "contents": [
-        {
-          "type": "text",
-          "text": "おすすめ結果",
-          "size": "xl",
-          "weight": "bold",
-          "align": "center",
-          "color": "#000000"
-        }
-      ]
-    },
-    "body": {
-      "type": "box",
-      "layout": "vertical",
-      "contents": [
-        {
-          "type": "box",
-          "layout": "vertical",
-          "margin": "xxl",
-          "spacing": "sm",
-          "contents": [
-            {
-              "type": "text",
-              "text": "ボタンを押してPDFをみる",
-              "weight": "bold",
-              "align": "center"
-            }
-          ]
-        },
-        {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "button",
-              "action": {
-                "type": "uri",
-                "label": "ダウンロード",
-                "uri": quote(file_url,safe=':/') #ここにpdfのurlを入れる
-              }
-            }
-          ]
-        }
-        
-      ]
-    },
-    "styles": {
-      "header": {
-        "backgroundColor": "#da70d6"
-      },
-      "hero": {
-        "backgroundColor": "#da70d6"
-      },
-      "footer": {
-        "backgroundColor": "#ba55d3"
-      }
-    }
-  }
-  flex_message_contents2 = {
     "type": "bubble",
     "header": {
       "type": "box",
@@ -124,15 +77,16 @@ def lambda_handler(event, context):
       "contents": [
         {
           "type": "text",
-          "text": "ボタンを押してPDFを見る",
+          "text": "下のボタンを押すことでおすすめ結果のページを見ることができます。尚この書類は3日で破棄されます。端末に保存するか印刷されることをおすすめします。",
           "weight": "bold",
-          "align": "center"
+          "align": "center",
+          "wrap": True
         },
         {
           "type": "button",
           "action": {
             "type": "uri",
-            "label": "ダウンロード",
+            "label": "PDFを見る",
             "uri": quote(file_url,safe=':/')
           }
         }
@@ -152,15 +106,13 @@ def lambda_handler(event, context):
       }
     }
   }
-  flex_message = FlexSendMessage(alt_text="質問", contents=flex_message_contents2)
+  flex_message = FlexSendMessage(alt_text="おすすめのスマホの提案", contents=flex_message_contents)
   # Flexメッセージを送信
   LINE_CHANNEL_ACCESS_TOKEN = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
   LINE_BOT_API = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
   response_line=LINE_BOT_API.reply_message(replyToken, flex_message)
   print(response_line)
-    
-  # No matching file found
-  return {'statusCode': 404, 'body': 'No matching PDF file found'}
+  return "success!"
 
 
 
