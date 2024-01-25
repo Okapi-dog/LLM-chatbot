f42e1fe88c6e43bd5ea989a5fafe519f6d9dff9c
Okapi Dog <150521381+Okapi-dog@users.noreply.github.com>
2024-01-25 20:18:42 +0900

    効率化+プロンプトエンジニアリング

--- docker/recommend.py
+++ docker/recommend.py
@@ -1,8 +1,11 @@
 import asyncio
+import nest_asyncio
+nest_asyncio.apply()
 import re
 from time import time
 #from config import dotenv_setting  # api_keyの読み込み
 from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain.schema import StrOutputParser
 from langchain.schema.document import Document
@@ -26,32 +29,12 @@ def convert_to_dict(input_str: str) -> dict[str, str]:
 
 async def generate_recommendations(request: str, answer_dict: dict[str, str]) -> list[str]:
     prompt = PromptTemplate.from_template(
-        """以下のrequestに基づいたおすすめのスマートフォンの情報がanswer_dictに格納されています。answer_dictの情報を基に、requestでユーザーが求める点を中心に推薦文を20字以内で3つ生成してください。内容はanswer_dictに書いてある内容のみにし、answer_dictに書いてない内容は絶対に含めないで下さい。推薦文に1.などは必要ありません。"@#$%^などと言った文字列は絶対に含めないでください。"
-        例は以下の通りです。例は要件に含めないでください。
+        """ユーザーの求める要件と推薦するスマートフォンの情報が下に格納されています。これらを基にスマートフォンの推薦文を必ず40字以内で3つ生成してください。内容は推薦するスマートフォンの情報に書いてある内容のみにし、推薦するスマートフォンの情報に書いてない内容は絶対に含めないで下さい。推薦文に1.などは必要ありません。"@#$%^などと言った文字列は絶対に含めないでください。
 
-        例:
-            request: OSのサポート期間が長く、iPhone 11 Proに近い重さで、原神を快適にプレイできるスマートフォンを教えてください。
-            answer_dict: "機種": "iPhone 12 Pro Max",
-                         "サイズ": "6.7インチ",
-                         "価格": "150000円",
-                         "CPU": "A14 Bionic",
-                         "メモリ": "6GB",
-                         "ストレージ": "256GB",
-                         "バッテリー": "3687mAh",
-                         "カメラ": "トリプルカメラ",
-                         "画面": "有機EL",
-                         "解像度": "2778x1284",
-                         "重さ": "226g",
-                         "OS": "iOS 14",
-                         "その他": "防水防塵"
-            response: OSのサポート期間がiOSのため長い
-                      iPhone 11 Proに近い重さの226g
-                      原神の快適なプレイが可能なCPU
-        例の終わり
-
-        request: {request}
-        answer_dict: {answer_dict}
-        response:
+        ユーザーの求める要件: {request}
+        推薦するスマートフォンの情報: {answer_dict}
+        推薦文は絶対に40文字以内で三つ生成し、改行で区切ってください。ユーザーの求める要件を推薦文に含めないでください。
+        推薦文:
         """,
     )
     model = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0)
@@ -62,126 +45,10 @@ async def generate_recommendations(request: str, answer_dict: dict[str, str]) ->
     recommendations = await chain.ainvoke({"request": request, "answer_dict": answer_dict})
     # 生成した推薦文を改行で区切ってリストに格納
     recommendations = [recommendation.strip() for recommendation in recommendations.split("\n", 2)]
-    return recommendations
-
-
-async def select_review(request: str, phone: str) -> list[str]:
-    review_dict =await get_reviews_dynamo(phone)
-    prompt = PromptTemplate.from_template(
-        """ユーザーの求める要件に最も適合しているレビューを二つレビュー番号で答えてください。製品情報を実機を触らずに述べているレビューの評価は低くなります。errorコードが入っているレビューや違う製品についてのレビューは不適合なレビューとします。レビュー番号は1,2,3,4,5のいずれかです。返答は数字のみ,で区切って答えてください。"
-
-        例:
-            ユーザーが求める要件: 予算: 5万円から8万円,重視するポイント: 長時間バッテリーを持つこと,主な目的: ゲームをプレイすること,好みのサイズ: 手にしっくりと収まる中くらいのサイズが好き
-            推薦機種: iPhone 12 Pro Max
-            レビュー1: レビューは例の為省略。
-            レビュー2: レビューは例の為省略。
-            レビュー3: レビューは例の為省略。
-            レビュー4: レビューは例の為省略。
-            レビュー5: レビューは例の為省略。
-            response: 1,3
-        例の終わり
-
-        ユーザーが求める要件: {request}
-        推薦機種: {phone}
-        レビュー1: {review1}
-        レビュー2: {review2}
-        レビュー3: {review3}
-        レビュー4: {review4}
-        レビュー5: {review5}
-        response: 
-        """,
-    )
-    model = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0)
-    model_gpt4 = ChatOpenAI(model="gpt-4-1106-preview", temperature=0)
-    output_parser = StrOutputParser()
-    chain = prompt | model | output_parser
-    # 推薦文の生成を非同期実行
-    review_num = await chain.ainvoke({"request": request, "phone": phone, "review1": review_dict["レビュー1"], "review2": review_dict["レビュー2"], "review3": review_dict["レビュー3"], "review4": review_dict["レビュー4"], "review5": review_dict["レビュー5"]})
-    # 生成した推薦文を改行で区切ってリストに格納
-    review_num = review_num.split(",")
-    review1=""
-    review2=""
-    review1_url=""
-    review2_url=""
-    for i in range(5):
-        if len(review_num) == 2:
-            if review_num[0] == str(i+1):
-                review1 = review_dict[f"レビュー{i+1}"]
-                review1_url = review_dict[f"レビュー{i+1}のurl"]
-            if review_num[1] == str(i+1):
-                review2 = review_dict[f"レビュー{i+1}"]
-                review2_url = review_dict[f"レビュー{i+1}のurl"]
-        elif len(review_num) == 1:
-            if review_num[0] == str(i+1):
-                review1 = review_dict[f"レビュー{i+1}"]
-                review1_url = review_dict[f"レビュー{i+1}のurl"]
-    return review1,review2,review1_url,review2_url
-    
-
-async def sum_review(request: str, answer_dict: dict[str, str]) -> dict[str,str]:
-    review1,review2,review1_url,review2_url = await select_review(request, answer_dict["機種"])
-    prompt = PromptTemplate.from_template(
-        """下のレビューを日本語で要約してください。要約する際にはユーザーが求める要件を読みユーザーが必要としていることを意識して要約してください。要約にはメリットとデメリットどちらも含めることができます。要約は50字以内でお願いします。"
-
-        ユーザーが求める要件: {request}
-        推薦機種: {phone}
-        レビュー: {review}
-        response: 
-        """,
-    )
-    model = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0)
-    model_gpt4 = ChatOpenAI(model="gpt-4-1106-preview", temperature=0)
-    output_parser = StrOutputParser()
-    chain = prompt | model | output_parser
-    # 推薦文の生成を非同期実行
-    review_sum1 = await chain.ainvoke({"request": request, "phone": answer_dict["機種"], "review": review1})
-    review_sum2 = await chain.ainvoke({"request": request, "phone": answer_dict["機種"], "review": review2})
-    # 生成した推薦文を改行で区切ってリストに格納
-    output={"review1":review_sum1,"review2":review_sum2,"review1_url":review1_url,"review2_url":review2_url}
-    return output
+    for i in range(len(recommendations)):
+        recommendations[i]=escape_tex_special_chars(recommendations[i])
         
-
-async def get_reviews_dynamo(phone: str) -> dict[str, str]:
-    # DynamoDB サービスリソースを初期化
-    dynamodb = boto3.resource('dynamodb')
-
-    # 操作するテーブルを指定
-    table = dynamodb.Table('sumdatabase')
-
-    # スキャン操作で使用するフィルタ条件
-    filter_condition = Attr('機種').contains(phone)
-
-    # 初期化
-    found_items = []
-    last_evaluated_key = None
-
-    while True:
-        # スキャン操作のパラメータ
-        scan_kwargs = {
-            'FilterExpression': filter_condition
-        }
-
-        # 前回のスキャンで LastEvaluatedKey がある場合、次のページからスキャンを続ける
-        if last_evaluated_key:
-            scan_kwargs['ExclusiveStartKey'] = last_evaluated_key
-
-        # スキャン操作を実行
-        response = table.scan(**scan_kwargs)
-
-        # 検索結果があるか確認
-        if response['Items']:
-            found_items.extend(response['Items'])
-            break  # 最初の項目が見つかったらループを終了
-
-        # 次のページがないか確認
-        last_evaluated_key = response.get('LastEvaluatedKey')
-        if not last_evaluated_key:
-            break  # 全てのページをスキャンし終わったらループを終了
-    if len(found_items) == 0:
-        return {}
-    else:
-        return found_items[0]
-    
+    return recommendations
 
 def get_all_sum_dynamodb() -> list[dict[str, str]]:
     # DynamoDB サービスリソースを初期化
@@ -212,6 +79,121 @@ def get_all_sum_dynamodb() -> list[dict[str, str]]:
     print("all:"+str(len(found_items)))
     return found_items
 
+def escape_tex_special_chars(text):
+    # List of TeX special characters that need to be escaped
+    special_chars = ['#', '$', '%', '&', '_', '{', '}', '~', '^', '\\']
+
+    # Escaping each special character
+    for char in special_chars:
+        # Using a backslash '\' before each special character
+        # The backslash itself needs to be escaped in Python strings, hence '\\'
+        if char != '\\':  # Handling backslash separately
+            text = text.replace(char, '\\' + char)
+        else:
+            text = text.replace(char, '\\textbackslash ')
+
+    return text
+
+
+async def select_review(review_dict_list:list[dict[str,str]],request: str, phone: str) -> list[str]:
+    review_dict =await get_reviews(review_dict_list,phone)
+    if review_dict == None:
+        return "","","",""
+    
+    prompt = PromptTemplate.from_template(
+        """ユーザーの求める要件に最も適合しているレビューを二つレビュー番号で答えてください。ただ単に製品情報を述べているレビューの評価は低くしてください。違う製品についてのレビューは不適合なレビューとします。レビュー番号は1,2,3,4,5のいずれかです。返答は数字のみ,で区切って二つ答えてください。"
+
+        例:
+            ユーザーが求める要件: 予算: 5万円から8万円,重視するポイント: 長時間バッテリーを持つこと,主な目的: ゲームをプレイすること,好みのサイズ: 手にしっくりと収まる中くらいのサイズが好き
+            推薦機種: iPhone 12 Pro Max
+            レビュー1: レビューは例の為省略。
+            レビュー2: レビューは例の為省略。
+            レビュー3: レビューは例の為省略。
+            レビュー4: レビューは例の為省略。
+            レビュー5: レビューは例の為省略。
+            response: 1,3
+        例の終わり
+
+        ユーザーが求める要件: {request}
+        推薦機種: {phone}
+        レビュー1: {review1}
+        レビュー2: {review2}
+        レビュー3: {review3}
+        レビュー4: {review4}
+        レビュー5: {review5}
+        response: 
+        """,
+    )
+    model = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0)
+    model_gpt4 = ChatOpenAI(model="gpt-4-1106-preview", temperature=0)
+    llm = OpenAI(model="gpt-3.5-turbo-instruct")
+    output_parser = StrOutputParser()
+    chain = prompt | model | output_parser
+    # 推薦文の生成を非同期実行
+    review_num = await chain.ainvoke({"request": request, "phone": phone, "review1": review_dict["レビュー1"], "review2": review_dict["レビュー2"], "review3": review_dict["レビュー3"], "review4": review_dict["レビュー4"], "review5": review_dict["レビュー5"]})
+    # 生成した推薦文を改行で区切ってリストに格納
+    print("review_num:"+str(review_num))
+    review_num = review_num.split(",")
+    review1=""
+    review2=""
+    review1_url=""
+    review2_url=""
+    print(review_dict)
+    for i in range(5):
+        if len(review_num) == 2:
+            if review_num[0] == str(i+1):
+                review1 = review_dict[f"レビュー{i+1}"]
+                review1_url = review_dict[f"レビュー{i+1}のurl"]
+            if review_num[1] == str(i+1):
+                review2 = review_dict[f"レビュー{i+1}"]
+                review2_url = review_dict[f"レビュー{i+1}のurl"]
+        elif len(review_num) == 1:
+            if review_num[0] == str(i+1):
+                review1 = review_dict[f"レビュー{i+1}"]
+                review1_url = review_dict[f"レビュー{i+1}のurl"]
+            print("一個しかレビューない")
+    return review1,review2,review1_url,review2_url
+    
+
+async def sum_review(review_dict_list:list[dict[str,str]],request: str, answer_dict: dict[str, str]) -> dict[str,str]:
+    review1,review2,review1_url,review2_url = await select_review(review_dict_list,request, answer_dict["機種"])
+    prompt = PromptTemplate.from_template(
+        """ユーザーが求める要件を考慮してレビューのみを日本語で要約してください。要約にはメリットとデメリットどちらも含めることができます。
+
+        ユーザーが求める要件: {request}
+        {phone}のレビュー: {review}
+        レビューの要約のみを50字以内で回答してください。ユーザーが求める要件を要約に含めないでください。
+        """,
+    )
+    model = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0)
+    model_gpt4 = ChatOpenAI(model="gpt-4-1106-preview", temperature=0)
+    llm = OpenAI(model="gpt-3.5-turbo-instruct")
+    output_parser = StrOutputParser()
+    chain = prompt | model | output_parser
+    # 推薦文の生成を非同期実行
+    review_sum1 = await chain.ainvoke({"request": request, "phone": answer_dict["機種"], "review": review1})
+    review_sum1=escape_tex_special_chars(review_sum1)
+    review_sum2 = await chain.ainvoke({"request": request, "phone": answer_dict["機種"], "review": review2})
+    review_sum2=escape_tex_special_chars(review_sum2)
+    # 生成した推薦文を改行で区切ってリストに格納
+    output={"review1":review_sum1,"review2":review_sum2,"review1_url":review1_url,"review2_url":review2_url}
+    if review1_url == "":
+        output["review1"] = "エラーによりレビューが取得できませんでした。"
+    if review2_url == "":
+        output["review2"] = "エラーによりレビューが取得できませんでした。"
+    return output
+
+
+async def get_reviews(review_dict_list:list[dict[str, str]],phone:str) -> dict[str,str]:
+    for review_dict in review_dict_list:
+        if phone in review_dict["機種"]:
+            return review_dict
+    
+    return None
+    
+
+
+
 
 async def process_answer(request: str, answer_dict: dict[str,str]) -> list[str]:
     # 推薦文の生成を非同期実行
@@ -223,11 +205,11 @@ async def process_answer(request: str, answer_dict: dict[str,str]) -> list[str]:
 
 async def process_answers(request: str, answers_dict: list[dict[str, str]]) -> (list[list[str]],list[dict[str,str]]):
 
-    sum_dynamodb=get_all_sum_dynamodb()
+    review_dict_list=get_all_sum_dynamodb()
 
     # 各回答に対して非同期タスクを作成する
     tasks_reccomend = [asyncio.create_task(process_answer(request, answer_dict)) for answer_dict in answers_dict]
-    tasks_review = [asyncio.create_task(sum_review(request, answer_dict)) for answer_dict in answers_dict]
+    tasks_review = [asyncio.create_task(sum_review(review_dict_list,request, answer_dict)) for answer_dict in answers_dict]
 
     # asyncio.gatherを使用して、すべてのタスクを並行実行する
     outcome = await asyncio.gather(*tasks_reccomend, *tasks_review)
